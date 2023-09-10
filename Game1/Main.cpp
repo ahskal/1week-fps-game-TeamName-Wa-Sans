388d3caba4fd0e08a2d00763b249cf01af040943
Min Gyeong HO <rudghgmrdls1@gmail.com>
2023-09-10 21:06:17 +0900

    메인작업 0버전입니다

--- Game1/Main.cpp
+++ Game1/Main.cpp
@@ -1,5 +1,7 @@
 #include "stdafx.h"
 #include "Main.h"
+#include "InGameScene.h"
+#include "LobbyScene.h"
 
 Main::Main()
 {
@@ -13,7 +15,14 @@ Main::~Main()
 
 void Main::Init()
 {
-	
+    //로비씬 나중에 중간중간 완성되가면서 체크합니다
+    //SCENE->AddScene("Lobby", new LobbyScene);
+
+    //씬 추가 현재는 임의로 인게임 작업을위해 인게임씬을 먼저둡니다 추후 수정할수있음
+    SCENE->AddScene("InGame", new InGameScene);
+
+    //add 씬에서 정해둔 키값으로 변환해서 보여줄씬 정합니다
+    SCENE->ChangeScene("InGame");
 }
 
 void Main::Release()
@@ -22,29 +31,28 @@ void Main::Release()
 
 void Main::Update()
 {
-    Camera::ControlMainCam();
-    ImGui::Begin("Hierarchy");
-
-    ImGui::End();
+    SCENE->Update();
 
 }
 
 void Main::LateUpdate()
 {
-   
+    SCENE->LateUpdate();
 }
+
 void Main::PreRender()
 {
+
 }
 
 void Main::Render()
 {
-
+    SCENE->Render();
 }
 
 void Main::ResizeScreen()
 {
-
+    SCENE->ResizeScreen();
 }
 
 int WINAPI wWinMain(HINSTANCE instance, HINSTANCE prevInstance, LPWSTR param, int command)
