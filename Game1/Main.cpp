c822a9901f67d377df2fd727d3846ea3a79e8f63
sieunny <113986641+sieunny@users.noreply.github.com>
2023-09-11 14:35:00 +0900

    Revert "12"
    This reverts commit 1914d04de27b5d681ddbbaa1fe58352910011479.

--- Game1/Main.cpp
+++ Game1/Main.cpp
@@ -10,7 +10,7 @@ Main::Main()
     cam1->LoadFile("Cam.xml");
     cam_first = Camera::Create();
     cam_first->LoadFile("Cam.xml");
-    Camera::main = cam1;//kk
+    Camera::main = cam1;
 
     cam1->viewport.x = 0.0f;
     cam1->viewport.y = 0.0f;
