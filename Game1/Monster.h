16c6ff50ad88885767f782750376b77d29ad0e23
Min Gyeong HO <rudghgmrdls1@gmail.com>
2023-09-17 16:32:27 +0900

    수정본

--- Game1/Monster.h
+++ Game1/Monster.h
@@ -5,7 +5,7 @@ enum class MonType
 	WALK
 };	//현재 정의만 해 놓은 상태(2023/09/16 16:30)
 
-class Monster
+class Monster : public UNIT
 {
 private:
 	Actor* monster;
@@ -26,7 +26,8 @@ public:
 	void Update();
 	void Render();
 	
-	void Collide(GameObject* ob);	//오브젝트 충돌 함수 *** 충돌할 물체에 따라 타입을 변환하세요 ***
-	void Chase(GameObject* player, Vector3 playerPos);	//플레이어 추적 함수 *** 플레이어 타입으로 변환하세요 ***
+	Actor* GetMonsterActor() const { return monster; }
+	
+	void Chase(Player* player);	//플레이어 추적 함수 *** 플레이어 타입으로 변환하세요 ***
 };
 
